5843a4ba929684474a21c24c7e49acc5d568c038
facutarcaya <facu.tarcaya@gmail.com>
2022-08-08 23:12:46 -0300

    Testing conn

--- app/src/main/java/com/example/senalar/HomeActivity.kt
+++ app/src/main/java/com/example/senalar/HomeActivity.kt
@@ -4,15 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.senalar.databinding.ActivityHomeBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.io.PrintWriter
+import java.net.Socket
+import java.net.UnknownHostException
+import java.util.*
 
 @androidx.camera.core.ExperimentalGetImage
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding : ActivityHomeBinding
 
+    // Server variables BORRAR
+    private lateinit var client: Socket
+    private lateinit var printwriter: PrintWriter
+    private lateinit var inferenceQueue: Queue<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        initializeCommToPC()
 
         initializeButtons()
     }
@@ -30,6 +45,28 @@ class HomeActivity : AppCompatActivity() {
 
         binding.cameraButton.setOnClickListener {
             startActivity(Intent(this, CameraActivity::class.java))
+        }
+    }
+
+    private fun initializeCommToPC() {
+        inferenceQueue = ArrayDeque<String>()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                client = Socket("172.23.128.1", 12345) //connect to server
+                printwriter = PrintWriter(client.getOutputStream(), true)
+
+                while (true) {
+                    inferenceQueue.poll()?.let { newWord ->
+                        printwriter.write(newWord) //write the message to output stream
+                        printwriter.flush()
+                    }
+                }
+            } catch (e: UnknownHostException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
 }
