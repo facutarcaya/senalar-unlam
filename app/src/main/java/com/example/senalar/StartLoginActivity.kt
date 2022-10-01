60348b651f63e18ff75ccc85602b781c6104418b
facutarcaya <facu.tarcaya@gmail.com>
2022-10-01 13:02:58 -0300

    testing

--- app/src/main/java/com/example/senalar/StartLoginActivity.kt
+++ app/src/main/java/com/example/senalar/StartLoginActivity.kt
@@ -1,11 +1,142 @@
 package com.example.senalar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.example.senalar.databinding.ActivityStartLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 class StartLoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityStartLoginBinding
+    private var mGoogleSignInClient: GoogleSignInClient? = null
+    private val RC_SIGN_IN = 9001
+    private var state: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityStartLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start_login)
+        setContentView(binding.root)
+
+        state = findViewById(R.id.txtEstado)
+
+        initializeButtons()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("481251153805-sjr3mpka5p9egd61ijrj35jpfb8hcq06.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        // [END configure_signin]
+
+        // [START build_client]
+        // Build a GoogleSignInClient with the options specified by gso.
+        // [END configure_signin]
+
+        // [START build_client]
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        // [END build_client]
+
+    }
+    override fun onStart() {
+        super.onStart()
+
+        // [START on_start_sign_in]
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if  (account != null) {
+            state?.text = "Sesión iniciada."
+            // TODO INIT UI
+        }
+        else{
+            state?.text = "Debe iniciar sesión."
+        }
+        // [END on_start_sign_in]
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Toast.makeText(this, requestCode.toString(), Toast.LENGTH_SHORT).show()
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+    // [END onActivityResult]
+
+    // [END onActivityResult]
+    // [START handleSignInResult]
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount = completedTask.getResult(ApiException::class.java)
+            if  (account != null)
+            {
+                state?.text = account.email
+            }
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account)
+        } catch (e: ApiException) {
+            Toast.makeText(this, e.statusCode.toString(), Toast.LENGTH_SHORT).show()
+
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            //Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            //updateUI(null)
+            state?.text = "Error al iniciar sesión.Reintente"
+        }
+    }
+    // [END handleSignInResult]
+
+    // [END handleSignInResult]
+    // [START signIn]
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    // [END signIn]
+
+    // [END signIn]
+    // [START signOut]
+    private fun signOut() {
+        mGoogleSignInClient!!.signOut()
+            .addOnCompleteListener(this) {
+                // [START_EXCLUDE]
+                //updateUI(null)
+                // [END_EXCLUDE]
+            }
+    }
+    // [END signOut]
+
+    // [END signOut]
+    // [START revokeAccess]
+    private fun revokeAccess() {
+        mGoogleSignInClient!!.revokeAccess()
+            .addOnCompleteListener(this) {
+                // [START_EXCLUDE]
+                //updateUI(null)
+                // [END_EXCLUDE]
+            }
+    }
+
+    private fun initializeButtons() {
+        binding.signInButton.setOnClickListener {
+            signIn()
+        }
+        binding.signOutButton.setOnClickListener {
+            signOut()
+        }
+        binding.disconnectButton.setOnClickListener {
+            revokeAccess()
+        }
     }
 }
