a78e3ada2658b6a2d579a2321151feddf611837a
facutarcaya <facu.tarcaya@gmail.com>
2022-10-03 21:33:33 -0300

    Fix client id

--- app/src/main/java/com/example/senalar/StartLoginActivity.kt
+++ app/src/main/java/com/example/senalar/StartLoginActivity.kt
@@ -32,7 +32,7 @@ class StartLoginActivity : AppCompatActivity() {
 
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestIdToken(getString(R.string.your_web_client_id))
             .requestEmail()
             .build()
 
